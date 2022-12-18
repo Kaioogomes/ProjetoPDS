83d51f7216ae42e5a1f6f176a1ce9ef1d3f1cf17
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-18 16:26:59 -0300

    Update Pessoa.hpp

--- include/Pessoa.hpp
+++ include/Pessoa.hpp
@@ -4,12 +4,14 @@
 #include <string>
 
 class Pessoa{
-    private:
+    protected:
         std::string _nome;
         std::string _cpf;
     
     public:
         Pessoa(std::string nome, std::string cpf);
+        std::string get_nome();
+        std::string get_cpf();
 };
 
 
