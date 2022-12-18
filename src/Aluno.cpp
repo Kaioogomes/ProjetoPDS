01f90594ac87f4f434bdba6df63e510337ea1a6f
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-18 16:24:44 -0300

    Update Aluno.cpp

--- src/Aluno.cpp
+++ src/Aluno.cpp
@@ -11,3 +11,11 @@ void Aluno::ativar_contrato(){
 void Aluno::desativar_contrato(){
     _contrato_ativo = false;
 }
+
+Treino *Aluno::get_treino(unsigned n_treino){
+    return _ficha.find(n_treino)->second;
+}
+
+void Aluno::modificar_treino(unsigned n_treino, Treino treino){
+    *(_ficha.find(n_treino)->second) = treino;
+}
