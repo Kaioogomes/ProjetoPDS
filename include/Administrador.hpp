8c8f394c2a28170dcdae9477bb40a71a0ef28e66
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-18 16:28:11 -0300

    Update Administrador.hpp

--- include/Administrador.hpp
+++ include/Administrador.hpp
@@ -2,14 +2,17 @@
 #define ADM_H
 
 #include "Pessoa.hpp"
+#include "Aluno.hpp"
 
 class Administrador: public Pessoa{
     private:
 
 
     public:
-        Administrador();
-
+        Administrador(std::string nome, std::string cpf);
+        void ativar_contrato(Aluno &aluno);
+        void desligar_aluno(Aluno &aluno);
+        
 
 };
 
