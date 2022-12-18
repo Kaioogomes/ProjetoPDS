29cab810eb6596112e6f6c008bbfd2602ac57863
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-18 16:24:14 -0300

    Update Pessoa.cpp

--- src/Pessoa.cpp
+++ src/Pessoa.cpp
@@ -1,3 +1,11 @@
 #include "Pessoa.hpp"
 
 Pessoa::Pessoa(std::string nome, std::string cpf):_nome(nome), _cpf(cpf){}
+
+std::string Pessoa::get_nome(){
+    return _nome;
+}
+
+std::string Pessoa::get_cpf(){
+    return _cpf;
+}
