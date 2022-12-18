c6b87410702c165ddbc903f0b6c365e324ef1280
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-18 16:26:29 -0300

    Update Professor.hpp

--- include/Professor.hpp
+++ include/Professor.hpp
@@ -14,7 +14,7 @@ class Professor: public Pessoa{
         Treino *novo_treino();
         std::string ficha_aluno(Aluno &aluno);
         void modificar_treino(Aluno &aluno);
-        void associar_ficha(Aluno &aluno, std::set<Treino *> ficha);
+        void associar_ficha(Aluno &aluno, std::map<unsigned, Treino *> ficha);
 };
 
 #endif   
