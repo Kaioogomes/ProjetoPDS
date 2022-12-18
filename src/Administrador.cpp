faf411a42790a7c631265e04583d913579d729ef
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-18 16:25:20 -0300

    Create Administrador.cpp

---
+++ src/Administrador.cpp
@@ -0,0 +1,11 @@
+#include "Administrador.hpp"
+
+Administrador::Administrador(std::string nome, std::string cpf): Pessoa(nome, cpf){}
+
+void Administrador::ativar_contrato(Aluno &aluno){
+    aluno.ativar_contrato();
+}
+
+void Administrador::desligar_aluno(Aluno &aluno){
+    aluno.desativar_contrato();
+}
