a301feb58083324be99999233fe3ae8d1883d1ee
Kaio Gomes <gomeskaio1402@gmail.com>
2022-12-20 13:22:04 -0300

    Update main

--- program/main.cpp
+++ program/main.cpp
@@ -4,21 +4,29 @@
 #include "Sistema.hpp"
 
 int main(){   
-    cout<<"Sistema Iniciado"<<endl;
-    for(;;){
-        switch(entrar_sistema()){
-            case 1:
-                unsigned matricula;
-                cout<<"Matrícula: ";
-                cin>>matricula;
-                //sistema_aluno();
-                break;
-            case 2:
-                sistema_professor();
-                break;
-            case 3:
-                sistema_administrador();
-                break;
-        }
-    }
+    Administrador adm;
+    Professor prof;
+    std::map<unsigned, Aluno> aluno_db;
+    std::map<unsigned, Exercicio> exercicios_db;
+    std::map<unsigned, Treino> treino_db;
+
+    inicializar_sistema(aluno_db, exercicios_db, treino_db); 
+
+    // cout<<"Sistema Iniciado"<<endl;
+    // for(;;){
+    //     switch(entrar_sistema()){
+    //         case 1:
+    //             unsigned matricula;
+    //             cout<<"Matrícula: ";
+    //             cin>>matricula;
+    //             //sistema_aluno();
+    //             break;
+    //         case 2:
+    //             sistema_professor();
+    //             break;
+    //         case 3:
+    //             sistema_administrador();
+    //             break;
+    //     }
+    // }
 }
