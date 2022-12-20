1487098c3a1e4fbd48277facf21f34ec29ecdb9a
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-20 01:22:37 -0300

    Update Professor.cpp

--- src/Professor.cpp
+++ src/Professor.cpp
@@ -1,3 +1,29 @@
-// #include "Professor.hpp"
+#include "Professor.hpp"
+#include <iostream>
 
-// Professor::Professor(std::string nome, std::string cpf):Pessoa(nome, cpf){}
+Professor::Professor(){};
+
+bool Professor::verificar_senha(){
+    std::string senha;
+    std::cout<<std::endl<<"Senha: ";
+    std::cin>>senha;
+    if(senha == _senha)
+        return true;
+    std::cout<<"Senha Incorreta"<<std::endl<<"1 - Tentar Novamente     2 - Voltar";
+    unsigned opcao;
+    std::cin>>opcao;
+    if(opcao == 1)
+        return verificar_senha();
+    if(opcao == 2)
+        return false;
+}
+
+//Exercicio *novo_exercicio();
+
+//Treino *novo_treino();
+
+//std::string ficha_aluno(Aluno &aluno);
+
+//void modificar_treino(Aluno &aluno);
+
+//void associar_ficha(Aluno &aluno, std::map<unsigned, Treino *> ficha);
