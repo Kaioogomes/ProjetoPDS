bafd30d7a84aaaf8a6799045f9312b69c6bad72e
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-21 15:50:33 -0300

    Update Sistema.hpp

--- include/Sistema.hpp
+++ include/Sistema.hpp
@@ -1,29 +1,32 @@
 #ifndef SISTEMA_H
 #define SISTEMA_H
 
-#include "Administrador.hpp"
+#include <map>
+
 #include "Aluno.hpp"
+#include "Administrador.hpp"
 #include "Professor.hpp"
-#include "Treino.hpp"
+#include "ExCardio.hpp"
+#include "ExMusculacao.hpp"
 
-#include <iostream>
-
-void inicializar_sistema(std::map<unsigned, Aluno>  &aluno_db,
-                         std::map<unsigned, ExCardio> &cardio_db,
-                         std::map<unsigned, ExMusculacao> &musculacao_db,
-                         std::map<unsigned, Treino> &treino_db);
-
-bool verificar_senha(Professor &professor);
-
-bool verificar_senha(Administrador &administrador);
-
-unsigned entrar_sistema();
-
-void sistema_aluno(Aluno &aluno);
-
-void sistema_professor();
-
-void sistema_administrador();
+class Sistema{
+    private:
+        Professor prof;
+        Administrador adm;
+        std::map<unsigned, Aluno>  aluno_db;
+        std::map<unsigned, ExCardio> cardio_db;
+        std::map<unsigned, ExMusculacao> musculacao_db;
+        std::map<unsigned, Treino> treino_db;
+    public:
+        Sistema_classe();
+        void inicializar_sistema();
+        bool verificar_senha_professor();
+        bool verificar_senha_administrador();
+        unsigned entrar_sistema();
+        void sistema_aluno(Aluno &aluno);
+        void sistema_professor();
+        void sistema_administrador();
+};
 
 
 #endif
