c45c0abd2e5896bf4b8665c1b452f6c7c9b04db8
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-22 17:31:19 -0300

    Update ExMusculacao.hpp

--- include/ExMusculacao.hpp
+++ include/ExMusculacao.hpp
@@ -10,22 +10,13 @@ class ExMusculacao: public Exercicio {
                      ExercicioBase *base,
                      unsigned series,
                      unsigned repeticoes
-                    //  std::string nome,
-                    //  unsigned series = 3,
-                    //  unsigned repeticoes = 10,
-                    //  tempo_t tempo = tempo_t(30)
                      );
 
         virtual ~ExMusculacao(){};
-        virtual std::string get_descricao() override;
-        // virtual bool operator==(const Exercicio &ex) override;
-        // virtual std::ostream& operator<<(const Exercicio &ex) override;
-        // virtual std::ostream& operator>>(Exercicio &ex) override;  
-
+        virtual std::string get_descricao() override;  
     private:
         unsigned _series;
         unsigned _repeticoes;
-        // unsigned _tempo_descanso;
 };
 
 
