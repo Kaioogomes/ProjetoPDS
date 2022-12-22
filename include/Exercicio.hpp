d4feab84de9e190b162fe575cde3cc3f38a629e7
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-22 17:30:51 -0300

    Update Exercicio.hpp

--- include/Exercicio.hpp
+++ include/Exercicio.hpp
@@ -7,35 +7,13 @@
 class Exercicio{
     protected:
         ExercicioBase *_base;
-        // std::string _nome;
-        // TipoExerc _tipo;
-        // std::string _instrucoes;   
-        // tempo_t _tempo_descanso;
-        // unsigned _series;
-        // unsigned _repeticoes;
-        // unsigned _tempo_descanso;
-
     public:
-        Exercicio(
-            ExercicioBase *base
-            // std::string nome,
-            // tempo_t tempo,
-            // TipoExerc tipo
-            // std::string instrucoes = 0
-            // unsigned series = 3, 
-            // unsigned repeticoes = 10,
-            // unsigned tempo_descanso = 30
-        );
+        Exercicio(ExercicioBase *base);
         virtual ~Exercicio();
-        // std::string get_nome();
-
         virtual std::string get_descricao(){return "";};
         ExercicioBase *get_exercicio_base();
         TipoExerc get_tipo();
-        // virtual bool operator==(const Exercicio &ex) = 0;
         bool operator<(const Exercicio &ex);
-        // virtual std::ostream& operator<<(const Exercicio &ex);
-        // virtual std::ostream& operator>>(Exercicio &ex) = 0;
 };
 
 #endif
