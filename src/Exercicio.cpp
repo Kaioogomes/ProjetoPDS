9623416e3f40f3f259aa50c020fc3f1939ae59c4
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-21 23:18:35 -0300

    Update Exercicio.cpp

--- src/Exercicio.cpp
+++ src/Exercicio.cpp
@@ -19,3 +19,7 @@ bool Exercicio::operator<(const Exercicio &ex){
 ExercicioBase *Exercicio::get_exercicio_base(){
     return _base;
 }
+
+TipoExerc Exercicio::get_tipo(){
+    return _base->get_tipo();
+}
