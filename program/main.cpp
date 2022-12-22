124ad2db23162c045589ef95e2a2d0d73b35254c
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-22 17:32:17 -0300

    Update main.cpp

--- program/main.cpp
+++ program/main.cpp
@@ -1,13 +1,9 @@
-// #include "Aluno.hpp"
-// #include "Professor.hpp"
-// #include "Administrador.hpp"
 #include "Sistema.hpp"
 #include <unistd.h>
 
 int main(){   
     Sistema sistema;
     sistema.leitura_arquivos();
-    sleep(15);
     sistema.inicializar_sistema();
 
     return 0;
