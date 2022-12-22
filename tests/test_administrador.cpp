8a7d2e40ca572e8ffe8d37e15333b2c8ae1f675a
arthurfreiman <117132790+arthurfreiman@users.noreply.github.com>
2022-12-21 23:36:29 -0300

    Update test_administrador.cpp

--- tests/test_administrador.cpp
+++ tests/test_administrador.cpp
@@ -11,9 +11,20 @@ TEST_CASE("Teste do construtor e da senha"){
 TEST_CASE("Teste de alterar situação do contrato de aluno"){
     Administrador adm;
     Aluno Paulo = Aluno ("Paulo", 1);
-    CHECK_EQ(Paulo.status_contrato(), true);
+    CHECK(Paulo.status_contrato());
     adm.desligar_aluno(Paulo);
-    CHECK_EQ(Paulo.status_contrato(), false);
+    CHECK_FALSE(Paulo.status_contrato());
     adm.religar_aluno(Paulo);
-    CHECK_EQ(Paulo.status_contrato(), true);
+    CHECK(Paulo.status_contrato());
+}
+
+TEST_CASE("Teste adicionar novo aluno"){
+    Administrador adm;
+    std::map<unsigned, Aluno *> aluno_db;
+    Aluno *Matheus = adm.novo_aluno("Matheus", 1);
+    aluno_db.emplace(1, Matheus);
+    Aluno *aluno = aluno_db.at(1);
+    CHECK(aluno->get_matricula());
+    CHECK_EQ(aluno->get_nome(), "Matheus");
+    CHECK_EQ(aluno->get_matricula(), 1);
 }
